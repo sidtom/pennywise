79cb5670c1d5bda24258752877e979c853b0510f
sidtom <sidneygmz@gmail.com>
2025-01-10 00:01:38 +0530

    basic css for calendar

--- src/components/dashboard/calendar/calendar.jsx
+++ src/components/dashboard/calendar/calendar.jsx
@@ -1,14 +1,11 @@
 import { Calendar } from '@mantine/dates';
 import "@mantine/dates/styles.css";
 import "./calendar.css";
-import { useEffect, useRef } from 'react';
-export default function DashboardCalendar() {
-  const calendarRef = useRef(null);
 
-  useEffect(() => {  console.log(calendarRef);},[calendarRef])
+export default function DashboardCalendar() {
   return (
     <div className="calendar-container">
-      <Calendar ref={calendarRef}/>
+      <Calendar />
     </div>
   );
 }
