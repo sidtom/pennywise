986fa422d50e8bfda3f0c04c898670517ca366b7
sidtom <sidneygmz@gmail.com>
2025-01-19 22:56:48 +0530

    dashboard early

--- src/components/dashboard/dashboard.jsx
+++ src/components/dashboard/dashboard.jsx
@@ -1,8 +1,128 @@
 import DashboardCalendar from "../calendar/calendar"
-export default function Dashboard() {
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import {
+  AppBar,
+  Toolbar,
+  Typography,
+  CssBaseline,
+  Drawer,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+  Box,
+  IconButton,
+} from "@mui/material";
+import MenuIcon from "@mui/icons-material/Menu";
+import DashboardIcon from "@mui/icons-material/Dashboard";
+import AnalyticsIcon from "@mui/icons-material/Analytics";
+import SettingsIcon from "@mui/icons-material/Settings";
+import "./dashboard.css";
+const drawerWidth = 240;
+
+function Dashboard() {
   return (
-   <>
-   <DashboardCalendar></DashboardCalendar>
-   </>
-  )
+    <Router>
+      <Box sx={{ display: "flex" }}>
+        <CssBaseline />
+        {/* Top AppBar */}
+        <AppBar
+          position="fixed"
+          sx={{ zIndex: (theme) => theme.zIndex.drawer + 1 }}
+        >
+          <Toolbar>
+            <IconButton
+              color="inherit"
+              edge="start"
+              sx={{ marginRight: 2 }}
+            >
+              <MenuIcon />
+            </IconButton>
+            <Typography variant="h6" noWrap>
+              Pennywise
+            </Typography>
+          </Toolbar>
+        </AppBar>
+
+        {/* Sidebar Drawer */}
+        <Drawer
+          variant="permanent"
+          sx={{
+            width: drawerWidth,
+            flexShrink: 0,
+            [`& .MuiDrawer-paper`]: {
+              width: drawerWidth,
+              boxSizing: "border-box",
+            },
+          }}
+        >
+          <Toolbar />
+          <Box sx={{ overflow: "auto" }}>
+            <List>
+              <ListItem disablePadding>
+                <ListItemButton component={Link} to="/">
+                  <ListItemIcon>
+                    <DashboardIcon />
+                  </ListItemIcon>
+                  <ListItemText primary="Dashboard" />
+                </ListItemButton>
+              </ListItem>
+              <ListItem disablePadding>
+                <ListItemButton component={Link} to="/analytics">
+                  <ListItemIcon>
+                    <AnalyticsIcon />
+                  </ListItemIcon>
+                  <ListItemText primary="Analytics" />
+                </ListItemButton>
+              </ListItem>
+              <ListItem disablePadding>
+                <ListItemButton component={Link} to="/settings">
+                  <ListItemIcon>
+                    <SettingsIcon />
+                  </ListItemIcon>
+                  <ListItemText primary="Settings" />
+                </ListItemButton>
+              </ListItem>
+            </List>
+          </Box>
+        </Drawer>
+
+        {/* Main Content */}
+        <Box
+          component="main"
+          sx={{
+            flexGrow: 1,
+            bgcolor: "background.default",
+            p: 3,
+            marginLeft: `${drawerWidth}px`,
+          }}
+        >
+          <Toolbar />
+          <Routes>
+            <Route
+              path="/"
+              element={
+                <DashboardCalendar />
+              }
+            />
+            <Route
+              path="/analytics"
+              element={
+                <Typography variant="h4">Analytics Placeholder</Typography>
+              }
+            />
+            <Route
+              path="/settings"
+              element={
+                <Typography variant="h4">Settings Placeholder</Typography>
+              }
+            />
+          </Routes>
+        </Box>
+      </Box>
+    </Router>
+  );
 }
+
+export default Dashboard;
