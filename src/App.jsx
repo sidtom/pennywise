488a3b81682cef689b6d8ac9b410f6ea425de5d3
sidtom <sidneygmz@gmail.com>
2025-02-11 23:11:29 +0530

    initial chart setups

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css'
 import Dashboard from './components/dashboard/dashboard'
 import '@mantine/core/styles.css';
+import '@mantine/charts/styles.css';
 import { MantineProvider } from '@mantine/core';
 function App() {
 
--- src/components/analytics/analyticsDashboard.jsx
+++ src/components/analytics/analyticsDashboard.jsx
@@ -1,14 +1,38 @@
-// import {
-//     Typography
-//   } from "@mui/material";
 import "./analyticsDashboard.css";
+import { AreaChart } from "@mantine/charts";
+import { expenses } from "../../mocks/expense";
 export default function AnalyticsDashboard() {
-    return (
-<div style={{ display: "flex", flexWrap: "wrap", gap: "10px" }}>
-    <div className="box" style={{ flex: "1 1 45%", border: "1px solid black", padding: "10px" }}>Box1</div>
-    <div className="box" style={{ flex: "1 1 45%", border: "1px solid black", padding: "10px" }}>Box2</div>
-    <div className="box" style={{ flex: "1 1 45%", border: "1px solid black", padding: "10px" }}>Box3</div>
-    <div className="box" style={{ flex: "1 1 45%", border: "1px solid black", padding: "10px" }}>Box4</div>
-</div>
-    );
+  const categories = [
+    ...new Set(
+      expenses.flatMap((expense) => expense.transactions.map((t) => t.category))
+    ),
+  ];
+  const series = categories.map((category, index) => ({
+    name: category,
+    color: `hsl(${index * 30}, 70%, 50%)`, // Generate a unique color for each category
+  }));
+
+  const chartData = expenses.map((expense) => {
+    const dataPoint = { date: expense.date };
+    expense.transactions.forEach((transaction) => {
+      dataPoint[transaction.category] = transaction.amount;
+    });
+    return dataPoint;
+  });
+  return (
+    <div style={{ display: "flex", flexWrap: "wrap", gap: "10px" }}>
+      <div className="box">
+        <AreaChart
+          h={300}
+          data={chartData}
+          dataKey="date"
+          series={series}
+          curveType="linear"
+        />
+      </div>
+      <div className="box">Box2</div>
+      <div className="box">Box3</div>
+      <div className="box">Box4</div>
+    </div>
+  );
 }
