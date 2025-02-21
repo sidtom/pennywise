d533273ac49351190e37f274748b42922a7e4187
sidtom <sidneygmz@gmail.com>
2025-02-21 14:42:26 +0530

    barchart code

--- src/components/analytics/analyticsDashboard.jsx
+++ src/components/analytics/analyticsDashboard.jsx
@@ -1,6 +1,5 @@
 import "./analyticsDashboard.css";
-import { AreaChart } from "@mantine/charts";
-import { DonutChart } from '@mantine/charts';
+import { AreaChart, BarChart, DonutChart } from "@mantine/charts";
 import { expenses } from "../../mocks/expense";
 export default function AnalyticsDashboard() {
   const categories = [
@@ -21,44 +20,47 @@ export default function AnalyticsDashboard() {
     return dataPoint;
   });
 
+  const barData = expenses.map(expense => ({
+    day: expense.date,  // Keeping the date as 'month' for consistency with the given format
+    "Total Spending": expense.totalAmount,
+  }));
   const categoryColors = {
-    "Food": "yellow.6",
-    "Alcohol": "teal.6",
-    "Misc": "gray.6",
-    "Shopping": "blue.6",
-    "Rent": "red.6",
-    "Petrol": "orange.6",
-    "Recharge": "pink.6",
-    "Lekshmy": "cyan.6",
+    Food: "yellow.6",
+    Alcohol: "teal.6",
+    Misc: "gray.6",
+    Shopping: "blue.6",
+    Rent: "red.6",
+    Petrol: "orange.6",
+    Recharge: "pink.6",
+    Lekshmy: "cyan.6",
     "Medical store": "green.6",
-    "Subscriptions": "purple.6",
-    "Swimming": "lime.6",
-    "Hotel": "violet.6",
+    Subscriptions: "purple.6",
+    Swimming: "lime.6",
+    Hotel: "violet.6",
     "Gym 3 months": "indigo.6",
-    "Vinai": "emerald.6",
-    "Bipin": "amber.6",
-    "Things": "rose.6",
-    "Jam": "fuchsia.6",
-    "Other": "gray.4"
+    Vinai: "emerald.6",
+    Bipin: "amber.6",
+    Things: "rose.6",
+    Jam: "fuchsia.6",
+    Other: "gray.4",
   };
-  
+
   const categoryTotals = {};
-  
-  expenses.forEach(expense => {
-    expense.transactions.forEach(transaction => {
+
+  expenses.forEach((expense) => {
+    expense.transactions.forEach((transaction) => {
       if (!categoryTotals[transaction.category]) {
         categoryTotals[transaction.category] = 0;
       }
       categoryTotals[transaction.category] += transaction.amount;
     });
   });
-  
-  const donutData = Object.keys(categoryTotals).map(category => ({
+
+  const donutData = Object.keys(categoryTotals).map((category) => ({
     name: category,
     value: categoryTotals[category],
-    color: categoryColors[category] || "gray.6" // Default color if not found
+    color: categoryColors[category] || "gray.6", // Default color if not found
   }));
-
 
   return (
     <div style={{ display: "flex", flexWrap: "wrap", gap: "10px" }}>
@@ -71,8 +73,23 @@ export default function AnalyticsDashboard() {
           curveType="linear"
         />
       </div>
-      <div className="box"><DonutChart style={{marginLeft:"35%",marginTop:"10%"}} data={donutData} /></div>
-      <div className="box">Box3</div>
+      <div className="box">
+        <DonutChart
+          style={{ marginLeft: "35%", marginTop: "10%" }}
+          data={donutData}
+        />
+      </div>
+      <div className="box">
+        <BarChart
+          h={300}
+          data={barData}
+          dataKey="day"
+          series={[
+            { name: "Total Spending", color: "violet.6" },
+          ]}
+          tickLine="y"
+        />
+      </div>
       <div className="box">Box4</div>
     </div>
   );
